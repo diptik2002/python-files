8e4fad51e5cf7bd4d00a220060b4c0e44d93e365
diptik2002 <108931403+diptik2002@users.noreply.github.com>
2022-07-18 16:19:10 +0530

    Delete 2Percentage Finder.py

--- 2Percentage Finder.py
+++
@@ -1,9 +0,0 @@
-print("Percentage Finder") 
-subject1=int(input("Enter your subject1 marks:"))
-subject2=int(input("Enter your subject2 marks:"))
-subject3=int(input("Enter your subject3 marks:"))
-subject4=int(input("Enter your subject4 marks:"))
-subject5=int(input("Enter your subject5 marks:"))
-total=subject1+subject2+subject3+subject4+subject5
-percentage=total/5  
-print("Your CGPA is:",percentage)
